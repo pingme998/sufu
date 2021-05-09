65e7d470c63dc17ad3fafea6821f99c8727d0b60
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-05-09 10:51:57 +0530

    Update entrypoint.sh

--- Essential-Files/entrypoint.sh
+++ Essential-Files/entrypoint.sh
@@ -18,5 +18,5 @@ sed "s|$OLD|$my_var|g" /Aria2Rclone.htpy >/Aria2Rclone.ipynb
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 jupyter serverextension enable voila --sys-prefix
 curl 'https://raw.githubusercontent.com/developeranaz/Aria2-Rclone-Remote-Uploader-HEROKU/main/Essential-Files/d/donate.developeranaz.txt'
-voila --port=$PORT --no-browser  /Aria2Rclone.ipynb --theme=dark
-#jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''
+#voila --port=$PORT --no-browser  /Aria2Rclone.ipynb --theme=dark
+jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''
